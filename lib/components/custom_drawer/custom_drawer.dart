18045a4fa6fe7ea0ac212518893ac6979a99be66
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-13 06:44:42 -0300

    feat: criando a tela base

---
+++ lib/components/custom_drawer/custom_drawer.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.6,
+      child: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
+import 'screens/base/base_screen.dart';
+
 Future<void> main() async {
+  await initializeParse();
   runApp(MyApp());
 
+}
+
+Future<void> initializeParse() async {
   await Parse().initialize(
     'NrFHP85AGpKoHwW79OVVjNCyeTBh7pbnCw2g1Hya',
     'https://parseapi.back4app.com/',
@@ -12,58 +18,18 @@ Future<void> main() async {
     autoSendSessionId: true,
     debug: true,
   );
-
-  // final category = ParseObject('Categories')
-  //   ..set('Title', 'Meias')
-  //   ..set('Position', 1);
-
-  // final response = await category.save();
-  // print(response.success);
-
-  // final  category = ParseObject('Categories')
-  //   ..objectId = 'EWfbygumHO'
-  //   ..set('Position', 3);
-
-  // final response = await category.save();
-  // print(response.success);
-
-  // final  category = ParseObject('Categories')
-  //   ..objectId = 'EWfbygumHO';
-
-  // category.delete();
-
-  // final response = await ParseObject('Categories').getObject('fjMJyVblap');
-  // if (response.success) {
-  //   print(response.result);
-  // }
-
-  // final response = await ParseObject('Categories').getAll();
-  // if (response.success) {
-  //   for (final object in response.result) {
-  //     print(object);
-  //   }
-  // }
-
-  // final query = QueryBuilder(ParseObject('Categories'));
-
-  // query.whereEqualTo('Position', 2);
-
-  // final response = await query.query();
-  // if (response.success) {
-  //   print(response.result);
-  // }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'XLO',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
