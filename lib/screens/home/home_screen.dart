60f75829eff712f07c17cccac53b80d5fb837ecd
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-14 10:31:20 -0300

    feat: criando a tela de login

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,9 +5,12 @@ import '../../components/custom_drawer/custom_drawer.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: CustomDrawer(),
-      appBar: AppBar(),
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        drawer: CustomDrawer(),
+        appBar: AppBar(),
+      ),
     );
   }
 }
