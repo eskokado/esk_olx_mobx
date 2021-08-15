0a33d1dddaae19e152aa2e313a1764113e980ed9
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-15 14:14:13 -0300

    feat: validando os campos do login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,8 +1,11 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
+import '../../stores/login_store.dart';
 import '../signup/signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
+  LoginStore loginStore = LoginStore();
 
   @override
   Widget build(BuildContext context) {
@@ -29,44 +32,47 @@ class LoginScreen extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Text(
-                      "Acessar com e-mail:", 
+                      "Acessar com e-mail:",
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[800],
-                    ),),
+                        fontSize: 16,
+                        color: Colors.grey[800],
+                      ),
+                    ),
                     Padding(
                       padding: EdgeInsets.only(left: 3, bottom: 4, top: 8),
-                      child: Text(
-                        "E-mail", 
-                        style: TextStyle(
-                          color: Colors.grey[900],
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                        )
-                      ),
+                      child: Text("E-mail",
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          )),
                     ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        isDense: true
-                      ),
-                      keyboardType: TextInputType.emailAddress,
+                    Observer(builder: (_) {
+                      return TextField(
+                        enabled: !loginStore.loading,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          isDense: true,
+                          errorText: loginStore.emailError,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: loginStore.setEmail,
+                      );
+                    }),
+                    const SizedBox(
+                      height: 16,
                     ),
-                    const SizedBox(height: 16,),
                     Padding(
                       padding: EdgeInsets.only(left: 3, bottom: 4),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "Senha",
-                            style: TextStyle(
-                              color: Colors.grey[800],
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700
-                            )
-                          ),
+                          Text("Senha",
+                              style: TextStyle(
+                                  color: Colors.grey[800],
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700)),
                           GestureDetector(
                             child: Text(
                               "Esqueceu sua senha?",
@@ -79,52 +85,60 @@ class LoginScreen extends StatelessWidget {
                           ),
                         ],
                       ),
-                    ),              
-                    TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        isDense: true
-                      ),
-                      obscureText: true,
                     ),
-                    Container(
-                      height: 40,
-                      margin: const EdgeInsets.only(top: 20, bottom: 12),
-                      child: RaisedButton(
-                        color: Colors.orange,
-                        child: Text('ENTRAR'),
-                        textColor: Colors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                    Observer(builder: (_) {
+                      return TextField(
+                        enabled: !loginStore.loading,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          isDense: true,
+                          errorText: loginStore.passwordError,
                         ),
-                        onPressed: (){},
-                      ),
-                    ),
+                        obscureText: true,
+                        onChanged: loginStore.setPassword,
+                      );
+                    }),
+                    Observer(builder: (_) {
+                      return Container(
+                        height: 40,
+                        margin: const EdgeInsets.only(top: 20, bottom: 12),
+                        child: RaisedButton(
+                          color: Colors.orange,
+                          disabledColor: Colors.orange.withAlpha(120),
+                          child: loginStore.loading
+                              ? CircularProgressIndicator(
+                                  valueColor:
+                                      AlwaysStoppedAnimation(Colors.white),
+                                )
+                              : Text('ENTRAR'),
+                          textColor: Colors.white,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          onPressed: loginStore.loginPressed,
+                        ),
+                      );
+                    }),
                     Divider(color: Colors.black),
                     Padding(
                       padding: EdgeInsets.symmetric(vertical: 8),
                       child: Wrap(
                         alignment: WrapAlignment.center,
                         children: [
-                          const Text(
-                            'Não tem uma conta? ', 
-                            style: TextStyle(fontSize: 16)
-                          ),
+                          const Text('Não tem uma conta? ',
+                              style: TextStyle(fontSize: 16)),
                           GestureDetector(
                             onTap: () {
                               Navigator.of(context).push(MaterialPageRoute(
-                                builder: (_) => SignupScreen()) 
-                              );
+                                  builder: (_) => SignupScreen()));
                             },
-                            child: Text(
-                              'Cadastrar-se',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 16, 
-                                color: Colors.purple,
-                              )
-                            ),
+                            child: Text('Cadastrar-se',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 16,
+                                  color: Colors.purple,
+                                )),
                           ),
                         ],
                       ),
