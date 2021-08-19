ac1715b940eab9ae2c70595b949e4325397da3be
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-19 07:37:33 -0300

    feat: obtendo a list de cidades e fazendo cache

--- lib/repositories/ibge_repository.dart
+++ lib/repositories/ibge_repository.dart
@@ -1,22 +1,47 @@
-﻿import 'package:dio/dio.dart';
+﻿import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/city.dart';
 import '../models/uf.dart';
 
 class IBGERepository {
   Future<List<UF>> getUFListFromApi() async {
+    final preferences = await SharedPreferences.getInstance();
+
+    if (preferences.containsKey('UF_LIST')) {
+      final jp = json.decode(preferences.get('UF_LIST'));
+      return jp.map<UF>((j) => UF.fromJson(j)).toList()
+        ..sort((UF a, UF b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+    }
+
     const endpoint =
         'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
     try {
       final response = await Dio().get<List>(endpoint);
 
-      final ufList = response.data.map<UF>((j) => UF.fromJson(j)).toList()
-        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+      preferences.setString('UF_LIST', json.encode(response.data));
 
-      return ufList;
+      return response.data.map<UF>((j) => UF.fromJson(j)).toList()
+        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
     } on DioError {
       return Future.error('Falha ao obter lista de Estados');
     }
   }
 
-  getCityListFromApi() {}
+  Future<List<City>> getCityListFromApi(UF uf) async {
+    final endpoint =
+        'http://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf.id}/municipios';
+
+    try {
+      final response = await Dio().get<List>(endpoint);
+
+      return response.data.map<City>((j) => City.fromJson(j)).toList()
+        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+    } on DioError {
+      return Future.error('Falha ao obter lista de Cidades do Estado ${uf.id}');
+    }
+  }
 }
