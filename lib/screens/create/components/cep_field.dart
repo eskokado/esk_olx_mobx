ea81ccc4ec9a2cfad70be18796218ddee964255a
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-20 06:37:09 -0300

    feat: criando o campo CEP

---
+++ lib/screens/create/components/cep_field.dart
@@ -0,0 +1,29 @@
+﻿import 'package:brasil_fields/brasil_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../../stores/cep_store.dart';
+
+class CepField extends StatelessWidget {
+  CepStore cepStore = CepStore();
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: cepStore.setCep,
+      keyboardType: TextInputType.number,
+      inputFormatters: [
+        FilteringTextInputFormatter.digitsOnly,
+        CepInputFormatter(),
+      ],
+      decoration: InputDecoration(
+        labelText: 'CEP *',
+        labelStyle: TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.grey,
+          fontSize: 18,
+        ),
+        contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+      ),
+    );
+  }
+}
