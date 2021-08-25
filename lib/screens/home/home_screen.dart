d612f5b212382e199fcf1f98e1d94b640f2c38ad
Edson Shideki Kokado <eskokado@gmail.com>
2021-08-25 06:31:00 -0300

    feat: criando o campo de pesquisa

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,15 +1,33 @@
 ﻿import 'package:flutter/material.dart';
 
 import '../../components/custom_drawer/custom_drawer.dart';
+import 'components/search_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
+  openSearch(BuildContext context) async {
+    final search =
+        await showDialog(context: context, builder: (_) => SearchDialog(
+          currentSearch: 'Edson Shideki Kokado',
+        ));
+    print(search);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
       child: Scaffold(
         drawer: CustomDrawer(),
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                openSearch(context);
+              },
+              icon: Icon(Icons.search),
+            ),
+          ],
+        ),
       ),
     );
   }
