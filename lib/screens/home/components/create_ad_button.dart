17948fefd13d48c3ea8dac239f405c601b789267
Edson Shideki Kokado <eskokado@gmail.com>
2021-09-10 05:53:21 -0300

    feat: Botão Animado de Inserir Anúncio

---
+++ lib/screens/home/components/create_ad_button.dart
@@ -0,0 +1,104 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../stores/page_store.dart';
+
+class CreateAdButton extends StatefulWidget {
+  final ScrollController scrollController;
+  CreateAdButton(this.scrollController);
+  @override
+  _CreateAdButtonState createState() => _CreateAdButtonState();
+}
+
+class _CreateAdButtonState extends State<CreateAdButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> buttonAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    buttonAnimation = Tween<double>(begin: 0, end: 66).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.4, 0.6),
+      ),
+    );
+
+    widget.scrollController.addListener(scrollChanged);
+  }
+
+  void scrollChanged() {
+    final s = widget.scrollController.position;
+    if (s.userScrollDirection == ScrollDirection.forward) {
+      controller.forward();
+    } else {
+      controller.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: buttonAnimation,
+      builder: (_, __) {
+        return FractionallySizedBox(
+          widthFactor: 0.7,
+          child: Container(
+            height: 50,
+            margin: EdgeInsets.only(
+              bottom: buttonAnimation.value,
+            ),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.white,
+                primary: Colors.orange,
+                onSurface: Colors.orange.withAlpha(120),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.camera_alt,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Anunciar agora',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                GetIt.I<PageStore>().setPage(1);
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
